6230a96922470a697182d67a68e2af1b2bbd8408
Vinayak Kapoor <krrishkapoor97@gmail.com>
2024-04-02 19:42:42 -0700

    Add events list logic

---
+++ pages/events/[...slug].js
@@ -0,0 +1,9 @@
+export default function FilteredEventsPage() {
+  return (
+    <div>
+        <h1>
+            Filtered events
+        </h1>
+    </div>
+  )
+}
---
+++ pages/events/index.js
@@ -0,0 +1,7 @@
+export default function AllEventsPage() {
+  return (
+    <div>
+        <h1>All the events</h1>
+    </div>
+  )
+}
